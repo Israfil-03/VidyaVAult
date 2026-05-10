92292d59d04346b2e547db2e31732f6ef8a785dc
Israfil Hoque <israfilhoque523@gmail.com>
2026-05-10 19:42:57 +0530

    feat: implement server entry point with graceful shutdown for database connections

--- server/src/checkPrisma.ts
+++
@@ -1,9 +0,0 @@
-import { prisma } from './prisma/client.js'
-
-async function check() {
-  console.log('Prisma keys:', Object.keys(prisma))
-  console.log('QuestionBankEntry:', (prisma as any).questionBankEntry)
-  process.exit(0)
-}
-
-check()
